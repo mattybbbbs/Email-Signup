6160a16a927c9230cbff9b45c70e6dbf89b59288
Matthew <mattyjforbes@gmail.com>
2021-09-13 11:57:17 +1000

    app JS uploaded but API keys removed

---
+++ app.js
@@ -0,0 +1,72 @@
+const bodyParser = require ('body-parser');
+const express = require ('express');
+const request = require ('request');
+const https = require('https');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+})
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const url = "";
+
+  const options = {
+    method: "POST",
+    auth: ""
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
+
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+})
+
+app.listen(port, () => {
+  console.log('Server running on port 3000')
+})
+
+
+//API Key 75054d24642ab77483c6c93e943ee1f7-us1
+
+//List ID e9cd9b65eb
+
+//'https://server.api.mailchimp.com/3.0/lists/
